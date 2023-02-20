dd415f9ad1cbb0b4781d295fe2ea942026632fc3
Zalman Izak <thezalmanian@gmail.com>
2023-02-20 06:12:03 -0800

    Add project files.

---
+++ InventoryManager/Form1.cs
@@ -0,0 +1,10 @@
+namespace InventoryManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ InventoryManager/Product.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManager
+{
+    /// <summary>
+    /// Represent's a product currently being sold
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// The product's stock keeping unit (Unique Identifier)
+        /// </summary>
+        [Key]
+        public string StockKeepingUnit { get; set; }
+
+        /// <summary>
+        /// The product's name
+        /// </summary>
+        [Required]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The product's retail price
+        /// </summary>
+        [Required]
+        public double Price { get; set; }
+    }
+
+    /// <summary>
+    /// Represent's a food item currently being sold
+    /// </summary>
+    public class Food : Product
+    {
+        /// <summary>
+        /// The food item's calorie count
+        /// </summary>
+        public int Calories { get; set; }
+
+        /// <summary>
+        /// A list of all allergy warnings
+        /// </summary>
+        public List<string> Allergies { get; set; }
+    }
+
+    /// <summary>
+    /// Represent's a book currently being sold
+    /// </summary>
+    public class Book : Product
+    {
+        /// <summary>
+        /// The book's International Standard Book Number
+        /// </summary>
+        public string ISBN { get; set; }
+
+        /// <summary>
+        /// The full name of the book's author
+        /// </summary>
+        public string Author { get; set; }
+    }
+}
