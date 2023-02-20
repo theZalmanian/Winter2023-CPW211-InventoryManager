9de764466ca330731f304fa6bb530e3944c8e316
Zalman Izak <thezalmanian@gmail.com>
2023-02-20 06:23:49 -0800

    Setup basic "Add Product" form layout

--- InventoryManager/AddProduct.Designer.cs
+++ InventoryManager/AddProduct.Designer.cs
@@ -28,19 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblSKU = new System.Windows.Forms.Label();
+            this.txtSKU = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.lblPrice = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblSKU
+            // 
+            this.lblSKU.AutoSize = true;
+            this.lblSKU.Location = new System.Drawing.Point(40, 36);
+            this.lblSKU.Name = "lblSKU";
+            this.lblSKU.Size = new System.Drawing.Size(31, 15);
+            this.lblSKU.TabIndex = 0;
+            this.lblSKU.Text = "SKU:";
+            // 
+            // txtSKU
+            // 
+            this.txtSKU.Location = new System.Drawing.Point(72, 33);
+            this.txtSKU.Name = "txtSKU";
+            this.txtSKU.Size = new System.Drawing.Size(129, 23);
+            this.txtSKU.TabIndex = 1;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(72, 62);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(129, 23);
+            this.txtTitle.TabIndex = 3;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(39, 65);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(32, 15);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "Title:";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(72, 91);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(129, 23);
+            this.txtPrice.TabIndex = 5;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(35, 94);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(36, 15);
+            this.lblPrice.TabIndex = 4;
+            this.lblPrice.Text = "Price:";
             // 
             // AddProduct
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(626, 409);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtSKU);
+            this.Controls.Add(this.lblSKU);
             this.Name = "AddProduct";
             this.Text = "AddProduct";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label lblSKU;
+        private TextBox txtSKU;
+        private TextBox txtTitle;
+        private Label lblTitle;
+        private TextBox txtPrice;
+        private Label lblPrice;
     }
 }
