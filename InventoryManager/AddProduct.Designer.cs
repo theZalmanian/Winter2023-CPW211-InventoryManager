706e2ec8215747cd6502f774d6955b531dd40d53
Zalman Izak <thezalmanian@gmail.com>
2023-02-20 07:06:30 -0800

    Changed "Add Product" form layout

--- InventoryManager/AddProduct.Designer.cs
+++ InventoryManager/AddProduct.Designer.cs
@@ -28,87 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblSKU = new System.Windows.Forms.Label();
             this.txtSKU = new System.Windows.Forms.TextBox();
             this.txtTitle = new System.Windows.Forms.TextBox();
-            this.lblTitle = new System.Windows.Forms.Label();
             this.txtPrice = new System.Windows.Forms.TextBox();
-            this.lblPrice = new System.Windows.Forms.Label();
+            this.grpProduct = new System.Windows.Forms.GroupBox();
+            this.grpFood = new System.Windows.Forms.GroupBox();
+            this.txtCalories = new System.Windows.Forms.TextBox();
+            this.txtAllergies = new System.Windows.Forms.TextBox();
+            this.grpProduct.SuspendLayout();
+            this.grpFood.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // lblSKU
-            // 
-            this.lblSKU.AutoSize = true;
-            this.lblSKU.Location = new System.Drawing.Point(40, 36);
-            this.lblSKU.Name = "lblSKU";
-            this.lblSKU.Size = new System.Drawing.Size(31, 15);
-            this.lblSKU.TabIndex = 0;
-            this.lblSKU.Text = "SKU:";
             // 
             // txtSKU
             // 
-            this.txtSKU.Location = new System.Drawing.Point(72, 33);
+            this.txtSKU.Location = new System.Drawing.Point(11, 20);
             this.txtSKU.Name = "txtSKU";
+            this.txtSKU.PlaceholderText = "SKU";
             this.txtSKU.Size = new System.Drawing.Size(129, 23);
             this.txtSKU.TabIndex = 1;
+            this.txtSKU.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtTitle
             // 
-            this.txtTitle.Location = new System.Drawing.Point(72, 62);
+            this.txtTitle.Location = new System.Drawing.Point(11, 49);
             this.txtTitle.Name = "txtTitle";
+            this.txtTitle.PlaceholderText = "Title";
             this.txtTitle.Size = new System.Drawing.Size(129, 23);
             this.txtTitle.TabIndex = 3;
-            // 
-            // lblTitle
-            // 
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Location = new System.Drawing.Point(39, 65);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(32, 15);
-            this.lblTitle.TabIndex = 2;
-            this.lblTitle.Text = "Title:";
+            this.txtTitle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtPrice
             // 
-            this.txtPrice.Location = new System.Drawing.Point(72, 91);
+            this.txtPrice.Location = new System.Drawing.Point(11, 78);
             this.txtPrice.Name = "txtPrice";
+            this.txtPrice.PlaceholderText = "Price";
             this.txtPrice.Size = new System.Drawing.Size(129, 23);
             this.txtPrice.TabIndex = 5;
+            this.txtPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // lblPrice
+            // grpProduct
             // 
-            this.lblPrice.AutoSize = true;
-            this.lblPrice.Location = new System.Drawing.Point(35, 94);
-            this.lblPrice.Name = "lblPrice";
-            this.lblPrice.Size = new System.Drawing.Size(36, 15);
-            this.lblPrice.TabIndex = 4;
-            this.lblPrice.Text = "Price:";
+            this.grpProduct.Controls.Add(this.txtSKU);
+            this.grpProduct.Controls.Add(this.txtPrice);
+            this.grpProduct.Controls.Add(this.txtTitle);
+            this.grpProduct.Location = new System.Drawing.Point(12, 12);
+            this.grpProduct.Name = "grpProduct";
+            this.grpProduct.Size = new System.Drawing.Size(151, 110);
+            this.grpProduct.TabIndex = 1;
+            this.grpProduct.TabStop = false;
+            this.grpProduct.Text = "Product Info";
+            // 
+            // grpFood
+            // 
+            this.grpFood.Controls.Add(this.txtCalories);
+            this.grpFood.Controls.Add(this.txtAllergies);
+            this.grpFood.Location = new System.Drawing.Point(12, 128);
+            this.grpFood.Name = "grpFood";
+            this.grpFood.Size = new System.Drawing.Size(151, 82);
+            this.grpFood.TabIndex = 6;
+            this.grpFood.TabStop = false;
+            this.grpFood.Text = "Food Info";
+            // 
+            // txtCalories
+            // 
+            this.txtCalories.Location = new System.Drawing.Point(11, 20);
+            this.txtCalories.Name = "txtCalories";
+            this.txtCalories.PlaceholderText = "Calories";
+            this.txtCalories.Size = new System.Drawing.Size(129, 23);
+            this.txtCalories.TabIndex = 1;
+            this.txtCalories.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtAllergies
+            // 
+            this.txtAllergies.Location = new System.Drawing.Point(11, 49);
+            this.txtAllergies.Name = "txtAllergies";
+            this.txtAllergies.PlaceholderText = "Allergies";
+            this.txtAllergies.Size = new System.Drawing.Size(129, 23);
+            this.txtAllergies.TabIndex = 3;
+            this.txtAllergies.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // AddProduct
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(626, 409);
-            this.Controls.Add(this.txtPrice);
-            this.Controls.Add(this.lblPrice);
-            this.Controls.Add(this.txtTitle);
-            this.Controls.Add(this.lblTitle);
-            this.Controls.Add(this.txtSKU);
-            this.Controls.Add(this.lblSKU);
+            this.ClientSize = new System.Drawing.Size(178, 390);
+            this.Controls.Add(this.grpFood);
+            this.Controls.Add(this.grpProduct);
             this.Name = "AddProduct";
             this.Text = "AddProduct";
+            this.grpProduct.ResumeLayout(false);
+            this.grpProduct.PerformLayout();
+            this.grpFood.ResumeLayout(false);
+            this.grpFood.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private Label lblSKU;
         private TextBox txtSKU;
         private TextBox txtTitle;
-        private Label lblTitle;
         private TextBox txtPrice;
-        private Label lblPrice;
+        private GroupBox grpProduct;
+        private GroupBox grpFood;
+        private TextBox txtCalories;
+        private TextBox txtAllergies;
     }
 }
