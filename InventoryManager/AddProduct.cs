a2c0cb5737b950267e6a9853f30f6c30d1845bb8
Zalman Izak <thezalmanian@gmail.com>
2023-02-20 07:35:22 -0800

    Implemented logic to display required inputs

--- InventoryManager/AddProduct.cs
+++ InventoryManager/AddProduct.cs
@@ -12,14 +12,47 @@ namespace InventoryManager
 {
     public partial class AddProduct : Form
     {
+        /// <summary>
+        /// Represent's the different types of Products available
+        /// </summary>
         public enum ProductTypes
         {
             Food, Book
         }
 
+        /// <summary>
+        /// Represent's the selected Product's type
+        /// </summary>
+        private ProductTypes _selectedType;
+
         public AddProduct(ProductTypes productType)
         {
             InitializeComponent();
+
+            // Setup switch for all Product types
+            switch (productType)
+            {
+                // If the product type is food
+                case ProductTypes.Food:
+                    // Display the food inputs
+                    grpFood.Visible = true;
+
+                    // Set the selected type as food
+                    _selectedType = ProductTypes.Food;
+                    break;
+
+                // If the product type is book
+                case ProductTypes.Book:
+                    // Display the book inputs
+                    grpBook.Visible = true;
+
+                    // Set the selected type as book
+                    _selectedType= ProductTypes.Book;
+                    break;
+
+                default:
+                    break;
+            }
         }
 
         private void AddProduct_Load(object sender, EventArgs e)
