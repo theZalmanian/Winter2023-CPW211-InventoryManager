01f3ee55351c9dc43974a42abd7a989bfb1d51f1
Zalman Izak <thezalmanian@gmail.com>
2023-02-20 09:39:15 -0800

    Setup DB Context for Products

---
+++ InventoryManager/InheritanceContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManager
+{
+    public class InheritanceContext : DbContext
+    {
+        public InheritanceContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=InventoryManager;Trusted_Connection=True;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Food> Foods { get; set; }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
